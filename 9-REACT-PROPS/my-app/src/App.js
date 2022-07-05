3b10ab76d156e3ce96e22a220a97954574651f0c
Nathan Ribeiro <nathanazevedo@gec.inatel.br>
2022-07-04 22:36:53 -0300

    update

--- 9-REACT-PROPS/my-app/src/App.js
+++ 9-REACT-PROPS/my-app/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 // Stylesheet
 import "./style.css";
+// Components
+import Contact from "./components/Contact";
+// Images
+import Cat1 from "./images/cat1.jpg"
+import Cat2 from "./images/cat2.jpg";
+import Cat3 from "./images/cat3.jpg";
+import Cat4 from "./images/cat4.jpg";
 
 export default function App() {
-  const date = new Date();
-  const hours = date.getHours()
-  let timeOfDay
-
-  if(hours<12){
-    timeOfDay = "morning"
-  }else if(hours >= 12 && hours < 17){
-    timeOfDay = "afternoon"
-  }else{
-    timeOfDay = "night"
-  }
-
   return (
-    <h1>Good {timeOfDay}!</h1>
+    <div>
+      <Contact img={Cat1} name="Mr. Cat" phone="(212) 555-1234" email="cat@gmail.com"/>
+      <Contact img={Cat2} name="Mr. Vladhovic" phone="(35) 999-000" email="vlad@catmail.com"/>
+      <Contact img={Cat3} name="Mr. Silva" phone="(51) 888-000" email="silvs@meow.com"/>
+      <Contact img={Cat4} name="Mr. Ronaldeski" phone="(37) 777-000" email="rona@gmail.com"/>
+    </div>
   );
 }
--- 9-REACT-PROPS/my-app/src/components/Contact.js
+++ 9-REACT-PROPS/my-app/src/components/Contact.js
@@ -1,9 +1,23 @@
 import React from "react";
 // Stylesheet
-import "./style.css";
+import "../style.css";
+// Images
+import PhoneIcon from "../images/phone-icon.svg";
+import EmailIcon from "../images/email-icon.svg";
 
-export default function App() {
+export default function Contact(props) {
   return (
-    <h1>Good {timeOfDay}!</h1>
+    <section className="contact--section">
+      <img className="contact--image" src={props.img} />
+      <h1 className="contact--name">{props.name}</h1>
+      <section className="contact--phone">
+        <img src={PhoneIcon} />
+        <h1>{props.phone}</h1>
+      </section>
+      <section className="contact--email">
+        <img src={EmailIcon} />
+        <h1>{props.email}</h1>
+      </section>
+    </section>
   );
 }
