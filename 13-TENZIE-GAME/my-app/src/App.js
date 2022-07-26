b2a8054490afeb0039577be5d846ca2fe22c38d3
Nathan Ribeiro <nathanazevedo@gec.inatel.br>
2022-07-25 23:02:39 -0300

    update project

---
+++ 13-TENZIE-GAME/my-app/src/App.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function App() {
+  return (
+    <main className="main--container">
+      <section className="scndmain--container"></section>
+    </main>
+  );
+}
+
+export default App;
