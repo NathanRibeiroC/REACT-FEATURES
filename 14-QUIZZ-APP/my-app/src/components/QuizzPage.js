53af058d8fa394622a73b188147925061c077396
Nathan Ribeiro <nathanazevedo@gec.inatel.br>
2022-08-05 17:04:58 -0300

    update project

--- 14-QUIZZ-APP/my-app/src/components/QuizzPage.js
+++ 14-QUIZZ-APP/my-app/src/components/QuizzPage.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "../index.css";
 import Question from "./Question";
+import { nanoid } from 'nanoid';
 
 export default function QuizzPage() {
   /**
    * State that represents the questions and answers
    */
-  const [questAndAnsw, setQuestAndAnsw] = React.useState([]);
-  //TODO: STOPPED HERE
-  const [questAndAnsw1, setQuestAndAnsw1] = React.useState([]);
-
+  const [questionStates, setQuestionStates] = React.useState([]);
 
   /**
    * Converts object type to array type in order
@@ -37,8 +35,30 @@ export default function QuizzPage() {
    */
   const parseFetchAsync = async () => {
     const otbObj = await otbFetch;
-    const fetchedResult = objectToArray(otbObj);
-    setQuestAndAnsw(fetchedResult);
+    const fetched = objectToArray(otbObj);
+    // console.log('FETCHED: ',fetched);
+    fetched.map((feRes)=>{
+      setQuestionStates((prev) => { return(
+        [...prev, 
+        {
+          id: nanoid(),
+          question: decodeURIComponent(feRes[1].question),
+          correctAlternative : decodeURIComponent(feRes[1].correct_answer),
+          incorrectAlternative1 : decodeURIComponent(feRes[1].incorrect_answers[0]),
+          incorrectAlternative2 : decodeURIComponent(feRes[1].incorrect_answers[1]),
+          incorrectAlternative3 : decodeURIComponent(feRes[1].incorrect_answers[2]),
+          isButton1Pressed: false,
+          isButton2Pressed: false,
+          isButton3Pressed: false,
+          isButton4Pressed: false,
+          button1Id: nanoid(),
+          button2Id: nanoid(),
+          button3Id: nanoid(),
+          button4Id: nanoid()
+        }]);
+      })
+    });
+    // setfetchResult(fetched);
   };
 
   /**
@@ -49,40 +69,53 @@ export default function QuizzPage() {
     parseFetchAsync();
   }, []);
 
-  /**
-   * Fisher-Yates Shuffle algorithm
-   * @param {array to be shuffled} array 
-   * @returns 
-   */
-  function shuffleQuestions(array){
-    let currentIndex = array.length,  randomIndex;
+  React.useEffect(function () {
+    console.log(questionStates)
+  }, [questionStates]);
 
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
+  function selectAnOption(btnId, key){
+    questionStates.map((quesRes)=>{
+      if(quesRes.id === key){
+        if(quesRes.button1Id === btnId){
+          setQuestionStates((prev) => ({
+            button1Id: [...prev, {button1Id: !prev.button1Id}]
+          }))
+        }
+        if(quesRes.button2Id === btnId){
+          console.log('BTN 2 CLICKED');
+        }
+        if(quesRes.button3Id === btnId){
+          console.log('BTN 3 CLICKED')
+        }
+        if(quesRes.button4Id === btnId){
+          console.log('BTN 4 CLICKED')
+        }
+      }
+    });
   }
 
-  const questions = questAndAnsw.map((square) => (
+  console.log('QUESTION STATE: ',questionStates);
+
+  const questions = questionStates.map((quesRes) => (
     <Question
-      answer1={"A"}
-      answer2={"B"}
-      answer3={"C"}
-      answer4={"D"}
-      question={decodeURIComponent(square[1].question)}
+      answer1C={quesRes.correctAlternative}
+      answer2F={quesRes.incorrectAlternative1}
+      answer3F={quesRes.incorrectAlternative2}
+      answer4F={quesRes.incorrectAlternative3}
+      butPressed1 = {quesRes.isButton1Pressed}
+      butPressed2 = {quesRes.isButton2Pressed}
+      butPressed3 = {quesRes.isButton3Pressed}
+      butPressed4 = {quesRes.isButton4Pressed}
+      btn1Id = {quesRes.button1Id}
+      btn2Id = {quesRes.button2Id}
+      btn3Id = {quesRes.button3Id}
+      btn4Id = {quesRes.button4Id}
+      selectAnOption={selectAnOption}
+      question={quesRes.question}
+      key={quesRes.id}
+      mainQuestionId={quesRes.id}
     />
   ));
-
-
 
   return (
     <section className="quizz--section">
