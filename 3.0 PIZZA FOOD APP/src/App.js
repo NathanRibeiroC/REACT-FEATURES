09172b25fcddb67529f9c352c04f4658e597caa9
Nathan Ribeiro <nathanazevedo@gec.inatel.br>
2022-09-11 22:33:54 -0300

    update

--- 3.0 PIZZA FOOD APP/src/App.js
+++ 3.0 PIZZA FOOD APP/src/App.js
@@ -1,30 +1,30 @@
-import React, { useState, Fragment } from "react";
-import "./App.css";
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
-import Card from "./components/UI/Card";
+import CartProvider from './store/CartProvider';
 
-const App = () => {
+function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
 
   const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
 
   return (
-    <Fragment>
-      {cartIsShown && <Cart onCloseCard={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
-        <Meals/>
+        <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
-};
+}
 
 export default App;
